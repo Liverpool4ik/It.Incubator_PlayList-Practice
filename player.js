983a7b81b9c4d0e7060fc020be01ce7a0be72213
Liverpool4ik <liverpool4ik@proton.me>
2024-03-31 22:55:36 +0300

    first commit

---
+++ player.js
@@ -0,0 +1,41 @@
+// data
+
+let playList = {
+	title: "Hip-Hop Hits",
+	coverImageUrl: "./img/coverImage.png",
+	tracksInfo: {
+		totalTracksCount: 4,
+		trackDurationInSeconds: 733,
+	},
+	tracks: [
+		{
+			coverImageUrl: "./img/image-1.png",
+			artistName: "Eminem",
+			songTitle: "Rap-God,",
+			fileUrl: "...",
+			isHot: false,
+		},
+
+		{
+			coverImageUrl: "./img/image-2.png",
+			artistName: "50 cent",
+			songTitle: "In da Club,",
+			fileUrl: "...",
+			isHot: false,
+		},
+		{
+			coverImageUrl: "./img/image-3.png",
+			artistName: "Eminem",
+			songTitle: "Rap-God,",
+			fileUrl: "...",
+			isHot: false,
+		},
+		{
+			coverImageUrl: "./img/image-4.png",
+			artistName: "Eminem",
+			songTitle: "Rap-God,",
+			fileUrl: "...",
+			isHot: false,
+		},
+	],
+};
