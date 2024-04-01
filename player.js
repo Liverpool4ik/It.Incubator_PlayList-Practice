476dbb13f8e4d311b7677cff123529103d163bad
Liverpool4ik <liverpool4ik@proton.me>
2024-04-01 12:33:11 +0300

    added mp3 files

--- player.js
+++ player.js
@@ -12,29 +12,33 @@ let playList = {
 			coverImageUrl: "./img/image-1.png",
 			artistName: "Eminem",
 			songTitle: "Rap-God,",
-			fileUrl: "...",
-			isHot: false,
+			fileUrl: "./musicTracks/track-1.mp3",
+			isHot: true,
 		},
 
 		{
 			coverImageUrl: "./img/image-2.png",
 			artistName: "50 cent",
-			songTitle: "In da Club,",
-			fileUrl: "...",
+			songTitle: "Rap-God,",
+			fileUrl: "./musicTracks/track-2.mp3",
+			isHot: false,
 		},
+
 		{
 			coverImageUrl: "./img/image-3.png",
-			artistName: "Eminem",
-			songTitle: "Rap-God,",
-			fileUrl: "...",
+			artistName: "DMX",
+			songTitle: "X Gone'Give It To Ya,",
+			fileUrl: "./musicTracks/track-3.mp3",
 			isHot: false,
 		},
 		{
 			coverImageUrl: "./img/image-4.png",
-			artistName: "Eminem",
-			songTitle: "Rap-God,",
-			fileUrl: "...",
+			artistName: "Eminem fet 50 cent, Cashis, Lloyd Banks ",
+			songTitle: "You don't know",
+			fileUrl: "./musicTracks/track-4.mp3",
 			isHot: false,
 		},
 	],
 };
+
+//render
